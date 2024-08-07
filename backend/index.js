bcf99a9bf0176379600918a5be687049a2d8b631
Anish949 <74363280+Anish949@users.noreply.github.com>
2024-08-07 12:30:29 +0545

    cors

--- backend/index.js
+++ backend/index.js
@@ -7,16 +7,13 @@ const router = require("./routes");
 
 const app = express();
 
-app.use(cors());
-
-app.all("*", function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
-  );
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-});
+app.use(
+  cors({
+    origin: ["https://gmhmobilehouse.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
