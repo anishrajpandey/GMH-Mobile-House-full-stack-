f4021321aa63a8ea005fbc47437556d91d96f311
Anish949 <74363280+Anish949@users.noreply.github.com>
2024-08-07 12:27:30 +0545

    cors

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,14 @@ const app = express();
 
 app.use(cors());
 
-// app.all("*", function (req, res) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
-//   );
-//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-// });
+app.all("*", function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+});
 
 app.use(express.json());
 app.use(cookieParser());
